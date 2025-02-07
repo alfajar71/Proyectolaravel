40cff93cbc737cab83b737d816a9977e1a633066
Rachid EL Fakhar <alfajar@hotmail.es>
2025-02-07 13:33:35 +0100

    segunda confirmacion

--- Radios/routes/web.php
+++ Radios/routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/equipo', function(){
+    return '<h1>devolucion de los equipos</h1>';
+
+});
+
+Route::get('/equipo/{id}', function($identificador){
+    return '<h1>Equipo</h1>' .$identificador;
+});
+
+Route::get('/equipo/{id}/comentario/{coment}', function($id, $comentario){
+    return'Equipo '.$id.', Excelente trabajo '.$comentario;
+    
+});
+
+
+Route::get('/usuario/{nombre?}', function($nombre = 'Rachid'){
+    return '<h1>Nombre </h1>' .$nombre;
+});
